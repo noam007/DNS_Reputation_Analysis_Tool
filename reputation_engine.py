0188db3f8f2a34d4bddfc515a019f503b4a7aae5
noam007 <82471020+noam007@users.noreply.github.com>
2026-08-09 21:52:25 +0300

    Update reputation_engine.py

--- reputation_engine.py
+++ reputation_engine.py
@@ -1,3 +1,9 @@
+# AsyncReputationClient
+# Asynchronous client for querying the Reputation API. 
+# It sends domain reputation requests concurrently using aiohttp, while using rate limiting and configuration settings to control API access and performance. 
+# It also integrates monitoring and result handling for tracking API responses and request status.
+
+
 import aiohttp
 import asyncio
 import time
@@ -7,7 +13,6 @@ from config import SETTINGS
 
 API_URL = "https://microcks.gin.dev.securingsam.io/rest/Reputation+API/1.0.0/domain/ranking/{domain}"
 HEADERS = {"Authorization": "Token I_am_under_stress_when_I_test"}
-
 
 class AsyncReputationClient:
     def __init__(self):
